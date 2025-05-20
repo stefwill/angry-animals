8dcca430c49d47be0272e59779a3f7305627e14c
Stefan Willoughby <me@stefwill.net>
2025-05-20 19:08:36 +1000

    04 - Animal Scene Challenge 05 - Animal Scene Solution

---
+++ scripts/animal.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+class_name Animal
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	pass # Replace with function body.
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	pass # Replace with function body.
